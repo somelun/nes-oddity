bf836d388cd95614461fac64d21803f8a5826b88
somelun <info@lunatic.su>
2021-04-14 21:19:22 +0100

    Start to implement CPU (LDA instruction)

--- cpu.zig
+++ cpu.zig
@@ -1,1 +1,83 @@
-pub const CPU = struct {};
+const std = @import("std");
+const expect = std.testing.expect;
+const assert = std.debug.assert;
+
+const Bus = @import("bus.zig").Bus;
+
+const StatusFlag = enum(u8) {
+    C = (1 << 0), // carry
+    Z = (1 << 1), // zero
+    I = (1 << 2), // interrupt disable
+    D = (1 << 3), // decimal
+    B = (1 << 4), // break
+    U = (1 << 5), // unused?
+    V = (1 << 6), // overflow
+    N = (1 << 7), // negative
+};
+
+pub const CPU = struct {
+    A: u8,
+    X: u8,
+    Y: u8,
+    status: u8,
+    SP: u8 = 0xFF, // stack pointer
+    PC: u16, // program counter
+
+    pub fn init() CPU {
+        return CPU{
+            .A = 0,
+            .X = 0,
+            .Y = 0,
+            .status = 0,
+            .PC = 0,
+        };
+    }
+
+    pub fn interpret(self: *CPU, commands: []const u8) void {
+        self.PC = 0;
+
+        for (commands) |value| {
+            self.PC += 1;
+
+            switch (value) {
+                0xA9 => { // LDA
+                    const param = commands[self.PC];
+                    self.PC += 1;
+
+                    self.A = param;
+                    if (self.A == 0) {
+                        self.status = self.status | 0b0000_0010;
+                    } else {
+                        self.status = self.status | 0b1111_1101;
+                    }
+
+                    if (self.A & 0b1000_0000 != 0) {
+                        self.status = self.status | 0b1000_0000;
+                    } else {
+                        self.status = self.status & 0b0111_1111;
+                    }
+                },
+
+                0x00 => { // BRK
+                    return;
+                },
+
+                else => {
+                    return;
+                },
+            }
+        }
+    }
+};
+
+pub fn main() !void {
+    //
+}
+
+test "0xA9_LDA_immidiate_load_data" {
+    var cpu = CPU.init();
+    cpu.interpret(&[_]u8{ 0xA9, 0x05, 0x00 });
+    expect(cpu.A == 0x05);
+    assert(cpu.status & 0b0000_0010 == 0b00);
+    assert(cpu.status & 0b1000_0000 == 0);
+}
