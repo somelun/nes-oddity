91829163f4bbec07c86433fbab13455b5c1b1c24
somelun <info@lunatic.su>
2021-04-18 18:53:44 +0100

    Implement TAX and INX instructions

--- src/cpu.zig
+++ src/cpu.zig
@@ -2,6 +2,8 @@ const std = @import("std");
 const expect = std.testing.expect;
 const assert = std.debug.assert;
 
+const print = @import("std").debug.print;
+
 const Bus = @import("bus.zig").Bus;
 
 const StatusFlag = enum(u8) {
@@ -38,18 +40,15 @@ pub const CPU = struct {
                     const param = commands[self.PC];
                     self.PC += 1;
 
-                    self.A = param;
-                    if (self.A == 0) {
-                        self.status = self.status | 0b0000_0010;
-                    } else {
-                        self.status = self.status | 0b1111_1101;
-                    }
+                    self.lda(param);
+                },
 
-                    if (self.A & 0b1000_0000 != 0) {
-                        self.status = self.status | 0b1000_0000;
-                    } else {
-                        self.status = self.status & 0b0111_1111;
-                    }
+                0xAA => { //TAX
+                    self.tax();
+                },
+
+                0xE8 => { //INX
+                    self.inx();
                 },
 
                 0x00 => { // BRK
@@ -57,10 +56,46 @@ pub const CPU = struct {
                 },
 
                 else => {
-                    return;
+                    continue;
                 },
             }
         }
+    }
+
+    fn updateZeroAndNegativeFlag(self: *CPU, value: u8) void {
+        self.updateZeroFlag(value);
+        self.updateNegativeFlag(value);
+    }
+
+    fn updateZeroFlag(self: *CPU, value: u8) void {
+        if (value == 0) {
+            self.status = self.status | 0b0000_0010;
+        } else {
+            self.status = self.status | 0b1111_1101;
+        }
+    }
+
+    fn updateNegativeFlag(self: *CPU, value: u8) void {
+        if (value & 0b1000_0000 != 0) {
+            self.status = self.status | 0b1000_0000;
+        } else {
+            self.status = self.status & 0b0111_1111;
+        }
+    }
+
+    fn lda(self: *CPU, value: u8) void {
+        self.A = value;
+        self.updateZeroAndNegativeFlag(self.A);
+    }
+
+    fn tax(self: *CPU) void {
+        self.X = self.A;
+        self.updateZeroAndNegativeFlag(self.X);
+    }
+
+    fn inx(self: *CPU) void {
+        self.X +%= 1;
+        self.updateZeroAndNegativeFlag(self.X);
     }
 };
 
@@ -76,4 +111,30 @@ test "0xA9_LDA_zero_flag" {
     var cpu = CPU.init();
     cpu.interpret(&[_]u8{ 0xA9, 0x00, 0x00 });
     assert(cpu.status & 0b0000_0010 == 0b10);
+}
+
+test "0xA9_LDA_negative_flag" {
+    var cpu = CPU.init();
+    cpu.interpret(&[_]u8{ 0xa9, 0xff, 0x00 });
+    assert(cpu.status & 0b1000_0000 == 0b1000_0000);
+}
+
+test "0xAA_TAX_move_a_to_x" {
+    var cpu = CPU.init();
+    cpu.A = 10;
+    cpu.interpret(&[_]u8{ 0xAA, 0x00 });
+    expect(cpu.X == 10);
+}
+
+test "INX_overflow" {
+    var cpu = CPU.init();
+    cpu.X = 0xFF;
+    cpu.interpret(&[_]u8{ 0xE8, 0xE8, 0x00 });
+    expect(cpu.X == 1);
+}
+
+test "5_ops_working_together" {
+    var cpu = CPU.init();
+    cpu.interpret(&[_]u8{ 0xA9, 0xC0, 0xAA, 0xE8, 0x00 });
+    expect(cpu.X == 0xc1);
 }
