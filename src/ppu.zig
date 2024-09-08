cd4892712c679b68be7c41e659a184af8a3edac3
Sasha <somelun@gmail.com>
2024-09-08 20:23:58 +0100

    Update PPU Status Register implementation

--- src/ppu.zig
+++ src/ppu.zig
@@ -197,17 +197,24 @@ const ControllerRegister = struct {
     // ---- ----
     // VPHB SINN
     // |||| ||||
-    // |||| ||++- Base nametable address
+    // |||| ||**- Base nametable address
     // |||| ||    (0 = $2000; 1 = $2400; 2 = $2800; 3 = $2C00)
-    // |||| |+--- VRAM address increment per CPU read/write of PPUDATA
+    // |||| ||
+    // |||| |*--- VRAM address increment per CPU read/write of PPUDATA
     // |||| |     (0: add 1, going across; 1: add 32, going down)
-    // |||| +---- Sprite pattern table address for 8x8 sprites
+    // |||| |
+    // |||| *---- Sprite pattern table address for 8x8 sprites
     // ||||       (0: $0000; 1: $1000; ignored in 8x16 mode)
-    // |||+------ Background pattern table address (0: $0000; 1: $1000)
-    // ||+------- Sprite size (0: 8x8 pixels; 1: 8x16 pixels)
-    // |+-------- PPU master/slave select
+    // ||||
+    // ||||
+    // |||*------ Background pattern table address (0: $0000; 1: $1000)
+    // |||
+    // ||*------- Sprite size (0: 8x8 pixels; 1: 8x16 pixels)
+    // ||
+    // |*-------- PPU master/slave select
     // |          (0: read backdrop from EXT pins; 1: output color on EXT pins)
-    // +--------- Generate an NMI at the start of the
+    // |
+    // *--------- Generate an NMI at the start of the
     //            vertical blanking interval (0: off; 1: on)
     //
     const Flags = enum(u8) {
@@ -285,15 +292,22 @@ const MaskRegister = struct {
     // ---- ----
     // BGRs bMmG
     // |||| ||||
-    // |||| |||+- Greyscale (0: normal color, 1: produce a greyscale display)
-    // |||| ||+-- 1: Show background in leftmost 8 pixels of screen, 0: Hide
-    // |||| |+--- 1: Show sprites in leftmost 8 pixels of screen, 0: Hide
-    // |||| +---- 1: Show background
+    // |||| |||*- Greyscale (0: normal color, 1: produce a greyscale display)
+    // |||| |||
+    // |||| ||*-- 1: Show background in leftmost 8 pixels of screen, 0: Hide
+    // |||| ||
+    // |||| |*--- 1: Show sprites in leftmost 8 pixels of screen, 0: Hide
+    // |||| |
+    // |||| *---- 1: Show background
     // ||||
-    // |||+------ 1: Show sprites
-    // ||+------- Emphasize red (green on PAL/Dendy)
-    // |+-------- Emphasize green (red on PAL/Dendy)
-    // +--------- Emphasize blue
+    // ||||
+    // |||*------ 1: Show sprites
+    // |||
+    // ||*------- Emphasize red (green on PAL/Dendy)
+    // ||
+    // |*-------- Emphasize green (red on PAL/Dendy)
+    // |
+    // *--------- Emphasize blue
     //
     const Flags = enum(u8) {
         Greyscale = (1 << 0),
@@ -348,66 +362,65 @@ const StatusRegister = struct {
     // ---- ----
     // VSO. ....
     // |||| ||||
-    // |||+-++++- Least significant bits previously written into a PPU register
+    // |||*-****- Least significant bits previously written into a PPU register
     // |||        (due to register not being updated for this address)
-    // ||+------- Sprite overflow. The intent was for this flag to be set
+    // ||
+    // ||*------- Sprite overflow. The intent was for this flag to be set
     // ||         whenever more than eight sprites appear on a scanline, but a
     // ||         hardware bug causes the actual behavior to be more complicated
     // ||         and generate false positives as well as false negatives; see
     // ||         PPU sprite evaluation. This flag is set during sprite
     // ||         evaluation and cleared at dot 1 (the second dot) of the
     // ||         pre-render line.
-    // |+-------- Sprite 0 Hit.  Set when a nonzero pixel of sprite 0 overlaps
+    // ||
+    // |*-------- Sprite 0 Hit.  Set when a nonzero pixel of sprite 0 overlaps
     // |          a nonzero background pixel; cleared at dot 1 of the pre-render
     // |          line.  Used for raster timing.
-    // +--------- Vertical blank has started (0: not in vblank; 1: in vblank).
+    // |
+    // *--------- Vertical blank has started (0: not in vblank; 1: in vblank).
     //            Set at dot 1 of line 241 (the line *after* the post-render
     //            line); cleared after reading $2002 and at dot 1 of the
     //            pre-render line.
     //
-    const Value = enum(u8) {
+    const Flags = enum(u8) {
         Unused1 = (1 << 0),
         Unused2 = (1 << 1),
         Unused3 = (1 << 2),
         Unused4 = (1 << 3),
         Unused5 = (1 << 4),
-        SpriteOverlow = (1 << 5),
+        SpriteOverflow = (1 << 5),
         SpriteZeroHit = (1 << 6),
         VBlankStarted = (1 << 7),
     };
 
-    value: u8 = 0,
+    flags: u8 = 0,
 
     pub fn init() StatusRegister {
         return StatusRegister{};
     }
 
     pub fn setSpriteOverflow(self: *StatusRegister) void {
-        self.value = self.value & @intFromEnum(Value.SpriteOverflow);
+        self.flags = self.flags & @intFromEnum(Flags.SpriteOverflow);
     }
 
     pub fn setSpriteZeroHit(self: *StatusRegister) void {
-        self.value = self.value & @intFromEnum(Value.SpriteZeroHit);
+        self.flags = self.flags & @intFromEnum(Flags.SpriteZeroHit);
     }
 
     pub fn setVBlankStarted(self: *StatusRegister) void {
-        self.value = self.value & @intFromEnum(Value.VBlankStarted);
+        self.flags = self.flags & @intFromEnum(Flags.VBlankStarted);
     }
 
     pub fn isSpriteOverflow(self: *StatusRegister) bool {
-        return (self.value & @intFromEnum(Value.SpriteOverflow)) == 1;
+        return (self.flags & @intFromEnum(Flags.SpriteOverflow)) == 1;
     }
 
     pub fn isSpriteZeroHit(self: *StatusRegister) bool {
-        self.value = (self.value & @intFromEnum(Value.SpriteZeroHit)) == 1;
+        return (self.flags & @intFromEnum(Flags.SpriteZeroHit)) == 1;
     }
 
     pub fn isVBlankStarted(self: *StatusRegister) bool {
-        self.value = (self.value & @intFromEnum(Value.VBlankStarted)) == 1;
-    }
-
-    pub fn snapshot(self: *StatusRegister) u8 {
-        return self.value;
+        return (self.flags & @intFromEnum(Flags.VBlankStarted)) == 1;
     }
 };
 
